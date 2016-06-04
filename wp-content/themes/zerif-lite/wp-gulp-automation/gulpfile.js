5d725855bfac17a8becb404700c5b9bc782d7fad
Hugo Cicarelli <hcicarelli@gmail.com>
2016-06-04 00:29:47 -0300

    Sass implementation

--- wp-content/themes/zerif-lite/wp-gulp-automation/gulpfile.js
+++ wp-content/themes/zerif-lite/wp-gulp-automation/gulpfile.js
@@ -2,8 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('./css/src/*.scss')
+    gulp.src('../css/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('../css'));
 });
-gulp.task('default', ['sass']);
+gulp.task('watch', function() {
+    gulp.watch('../css/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
